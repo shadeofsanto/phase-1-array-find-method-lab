de3763f61d68e5321b86b7f618ea637370740580
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-05-05 14:02:19 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+// will loop through records and find the superbowl wins 'w'
+function superbowlWin(broncoRecord){
+    const wins = broncoRecord.find(superbowlResults => superbowlResults.result === "W");
+    return wins ? wins.year : undefined;
+}
